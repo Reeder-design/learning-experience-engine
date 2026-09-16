66a31e79f5c9bf82311988d332985cf6777f2937
Reeder-design <haleyreeder9@gmail.com>
2026-09-15 17:32:50 -0700

    Expand Rise inspector summary output

--- tools/rise-inspector/lib/summary.js
+++ tools/rise-inspector/lib/summary.js
@@ -1,3 +1,29 @@
+function sortedEntries(object) {
+  return Object.entries(object || {}).sort((a, b) => b[1] - a[1]);
+}
+
 module.exports = function summary(data) {
-  return 'Rise Inspector: ' + data.course.title;
+  const lines = [];
+  lines.push('');
+  lines.push('RISE INSPECTOR');
+  lines.push('==============');
+  lines.push('');
+  lines.push('Course: ' + data.course.title);
+  lines.push('Lessons: ' + data.course.lessonCount);
+  lines.push('Content blocks: ' + data.course.totalBlocks);
+  lines.push('Assessment questions: ' + data.course.totalQuestions);
+  lines.push('Assets: ' + data.assets.total);
+  lines.push('');
+  lines.push('LESSONS');
+  lines.push('-------');
+
+  data.lessons.forEach((lesson) => {
+    lines.push(lesson.number + '. ' + lesson.title + ' [' + lesson.type + ']');
+    sortedEntries(lesson.itemTypes).forEach(([type, count]) => {
+      lines.push('   ' + count + ' x ' + type);
+    });
+  });
+
+  lines.push('');
+  return lines.join('\n');
 };
